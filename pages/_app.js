c72eb388484475f730af2f52248621c98d51d6b1
adityas-opse <85329735+adityas-ops@users.noreply.github.com>
2023-08-03 00:02:34 +0530

    add footer

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
 import '@/styles/globals.css'
 import '@/styles/fonts.css'
 import Header from '@/components/Header'
+import Footer from '@/components/Footer'
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Header/>
       <Component {...pageProps} />
+      <Footer/>
     </>
   )
 }
