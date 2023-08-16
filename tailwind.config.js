287417bcac259af6d1af950679fda99fce36d2fe
adityas-opse <85329735+adityas-ops@users.noreply.github.com>
2023-08-16 11:50:04 +0530

    adding teenagers page

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,9 @@ module.exports = {
           'square-light':['Square Sans Pro light','Sans Sarif'],
           'square-regular':['Square Sans Pro Regular','Sans Sarif'],
           'square-medium':['Square Sans Pro medium','Sans Sarif'],
-          'Helvetica':['Helvetica','Sans Sarif']
+          'Helvetica':['Helvetica','Sans Sarif'],
+          'Inter':['Inter', 'sans-serif'],
+          'Atkinson':['Atkinson Hyperlegible', 'sans-serif']
       }
     },
   },
