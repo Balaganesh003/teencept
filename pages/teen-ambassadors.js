c2e8cc9c63bc3373f5e075d5959ef88ee7aab013
adityas-opse <85329735+adityas-ops@users.noreply.github.com>
2023-08-17 18:34:30 +0530

    init teen-ambassadors page

---
+++ pages/teen-ambassadors.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function TeenAmbassadors() {
+  return (
+   <>
+    <div className='h-[75vh] w-full bg-red-500 flex items-center justify-center'>
+    <div>
+      
+    </div>
+    </div>
+   </>
+  )
+}
+
+export default TeenAmbassadors
